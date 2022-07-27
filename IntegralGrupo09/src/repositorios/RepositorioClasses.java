fe4bc60b92c73a81bb0dcb563220876a33db00c3
thcastilho <thiagocesaralmeida@gmail.com>
2022-07-27 12:55:34 -0300

    1 more att

--- IntegralGrupo09/src/repositorios/RepositorioClasses.java
+++ IntegralGrupo09/src/repositorios/RepositorioClasses.java
@@ -1,5 +1,13 @@
 package repositorios;
 
+import modelo.Classe;
+
 public interface RepositorioClasses {
-    
+    void cadastrar(Classe classe);
+
+    void atualizar(Classe classe);
+
+    void gerarCodigo();
+
+    void gerarCodigoUML();
 }
